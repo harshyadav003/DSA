425afa5a6550032b214fd9eff6ed7861a93f50f5
HARSH YADAV <hyadav4440@gmail.com>
2026-08-10 19:59:34 +0530

    Time: 14 ms (29.68%), Space: 11.1 MB (52.16%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
+        //m1:
         // int cnt=0;
         // int n=s.size();
         // for(int i=0;i<n;i++){
@@ -12,21 +13,31 @@ public:
         // }
         // return cnt;
 
-        int l=0;
-        int r=0;
-        int n=s.size();
+        // m2:
+        // int l=0;
+        // int r=0;
+        // int n=s.size();
+        // int cnt=0;
+        // vector<int> hash(3);
+        // while(r<n){
+        //     hash[s[r]-'a']++;
+        //     while(hash[0] > 0 && hash[1] > 0 && hash[2] > 0){  
+        //     cnt += (n-r);
+
+        //    hash[s[l]-'a']--;
+        //    l++;
+        // }
+        //     r++;
+
+        // }
+        // return cnt;
+
+        // m3:
+       vector<int> lastseen(3, -1);
         int cnt=0;
-        vector<int> hash(3);
-        while(r<n){
-            hash[s[r]-'a']++;
-            while(hash[0] > 0 && hash[1] > 0 && hash[2] > 0){  
-            cnt += (n-r);
-
-           hash[s[l]-'a']--;
-           l++;
-        }
-            r++;
-
+        for(int i=0;i<s.size();i++){
+            lastseen[s[i]-'a']=i;
+            if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1) cnt+=(1+min(lastseen[0],min(lastseen[1],lastseen[2])));
         }
         return cnt;
     }
