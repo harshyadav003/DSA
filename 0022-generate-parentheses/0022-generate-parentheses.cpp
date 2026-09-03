ece8523b231d4e13b168e667318adc1a280af91d
HARSH YADAV <hyadav4440@gmail.com>
2026-09-04 02:47:14 +0530

    Time: 4 ms (31.06%), Space: 15.8 MB (38.92%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,24 +1,50 @@
+// class Solution {
+// public:
+//     void generate(int n, string curr, vector<string>& ans){
+//         int open = count(curr.begin(), curr.end(), '(');
+//         int close = count(curr.begin(), curr.end(), ')');
+
+//         if (open == n && close == n) {
+//             ans.push_back(curr);
+//             return;
+//         }
+   
+//        if(open < n)  generate(n,curr+"(",ans);
+        
+//        if( close< open ){
+//         generate(n,curr+")",ans);
+//     }
+   
+// }
+//     vector<string> generateParenthesis(int n) {
+//         string curr; vector<string> ans;
+//         generate(n,curr,ans);
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    void generate(int n, string curr, vector<string>& ans){
-        int open = count(curr.begin(), curr.end(), '(');
-        int close = count(curr.begin(), curr.end(), ')');
+    void generate(int n, string curr, int open ,int close,vector<string>& ans){
 
-        if (open == n && close == n) {
+
+        if (curr.size()==2*n) {
             ans.push_back(curr);
             return;
         }
    
-       if(open < n)  generate(n,curr+"(",ans);
+       if(open < n)  generate(n,curr+"(",open+1,close,ans);
         
        if( close< open ){
-        generate(n,curr+")",ans);
+        generate(n,curr+")",open,close+1,ans);
     }
    
 }
     vector<string> generateParenthesis(int n) {
         string curr; vector<string> ans;
-        generate(n,curr,ans);
+        generate(n,curr,0,0,ans);
         return ans;
     }
 };
