9c1c423aafd1c77723765ecee94ae03570900901
HARSH YADAV <hyadav4440@gmail.com>
2026-09-07 22:34:56 +0530

    Time: 0 ms (100%), Space: 9.2 MB (60.76%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,6 +1,39 @@
+// class Solution {
+// public:
+//   void fn(int i,int j,string digit,string s,string combos[],vector<string> &ans){
+        
+//          if(i==digit.size()){
+//             ans.push_back(s);
+//             return;
+//          }
+       
+//         int d = digit[i] - '0';
+
+//         if(j == combos[d].size()) { return; }
+
+//          s.push_back(combos[d][j]);
+//          fn(i+1,0,digit,s,combos,ans);
+//          s.pop_back();
+
+//          fn(i,j+1,digit,s,combos,ans); 
+
+
+//   }
+//     vector<string> letterCombinations(string digits) {
+//         // string s = "";
+//         string combos[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+//         vector<string> ans;
+//         fn(0,0,digits,"",combos,ans);
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
-  void fn(int i,int j,string digit,string s,string combos[],vector<string> &ans){
+  void fn(int i,string digit,string s,string combos[],vector<string> &ans){
         
          if(i==digit.size()){
             ans.push_back(s);
@@ -9,21 +42,17 @@ public:
        
         int d = digit[i] - '0';
 
-        if(j == combos[d].size()) { return; }
-
-         s.push_back(combos[d][j]);
-         fn(i+1,0,digit,s,combos,ans);
-         s.pop_back();
-
-         fn(i,j+1,digit,s,combos,ans);
+        for(int j=0;j<combos[d].size();j++){
+            fn(i+1,digit,s + combos[d][j],combos,ans);
+        }
 
 
   }
     vector<string> letterCombinations(string digits) {
-        string s = "";
+        // string s = "";
         string combos[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         vector<string> ans;
-        fn(0,0,digits,s,combos,ans);
+        fn(0,digits,"",combos,ans);
         return ans;
     }
 };
